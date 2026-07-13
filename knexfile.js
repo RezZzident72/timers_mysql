75922876442da64bd49223bc9478bc59e91c77c9
RezZzident72 <pavel.belkin@yandex.ru>
2026-07-13 23:47:37 +0500

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,25 @@
 require("dotenv").config();
 
+const connection = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: true,
+};
+
 module.exports = {
   development: {
     client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
+    connection: connection,
+    migrations: {
+      directory: "./migrations",
     },
+  },
+  production: {
+    client: "postgresql",
+    connection: connection,
     migrations: {
       directory: "./migrations",
     },
